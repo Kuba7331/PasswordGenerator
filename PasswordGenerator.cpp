341ac23354f548a028632a043165f9723d38cfef
Kuba7331 <kuba@kamionka.eu>
2021-07-01 15:17:56 +0200

    Third update, which adds a possibility to search for network service, and it returns password, which is connected to the name of network service.

--- PasswordGenerator.cpp
+++ PasswordGenerator.cpp
@@ -11,9 +11,15 @@ class Password {
 private:
 	int choice; 
 public:
-	string password, line, platformName;
-	string blank = "";
+	string password, line, platformName, network_service, network_service2;
+    string blank = "";
+	string* passwordPointer = &password;
+	string* networkServicePointer = &network_service;
 	int length;
+	int none = 0;
+	int lineNumber = 1;
+	int x = 0;
+	int maxTabelSize = 999;
 	char chars[72] = { 'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','r','s','t','u','w','x','x','y','z','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','R','S','T','U','W','X','X','Y','Z' ,'0','1','2','3','4','5','6','7','8','9','.','!','@','#','$','%','^','&','*','(',')' };
 	int getLength() {
 		return length;
@@ -23,6 +29,12 @@ public:
 	}
 	string getPlatformName() {
 		return platformName;
+	}
+	string getNetworkService() {
+		return network_service;
+	}
+	void setNetworkService(string network_service) {
+		this->network_service = network_service;
 	}
 	void setPlatformName(string platformName) {
 		this->platformName = platformName;	
@@ -42,6 +54,19 @@ public:
 		setPassword(password);
 	}
 	int gettingData(int var) {                                  //ogolna funkcja do pobierania jakichkolwiek danych, z obsluga bledow
+		while (true) {
+			if (!(cin >> var)) {
+				cin.clear();
+				cin.ignore();
+				cout << "Wystapil blad, podczas pobierania danych, prosze podac dane ponownie!" << endl;
+				getchar(); getchar();
+				system("cls");
+				continue;
+			}
+			return var;
+		}
+	}
+	string gettingDataString(string var) {                                  //ogolna funkcja do pobierania jakichkolwiek danych, z obsluga bledow, o typie string
 		while (true) {
 			if (!(cin >> var)) {
 				cin.clear();
@@ -87,6 +112,7 @@ public:
 				break;
 			case 2:
 				setPassword(blank);
+
 				break;
 			}
 			return getPassword();
@@ -135,6 +161,73 @@ public:
 			system("cls");
 			return getPassword();
 		}
+		void readAndGetTwoLinesFromFile() {
+			fstream file3;
+			file3.open("password.txt", ios::in);
+			if (!(file3.good() == true)) {
+				cout << "Wystapil problem z otwarciem pliku!" << endl;
+				cout << "Prosze nacisnac jakikolwiek przycisk, poza przyciskami znakow bialych, aby wrocic do menu." << endl;
+				getchar(); getchar();
+				system("cls");
+			}
+
+				passwordPointer = new string[maxTabelSize];
+				networkServicePointer = new string[maxTabelSize];
+				while (getline(file3, line)) {
+					switch (lineNumber) {
+					case 1:
+						networkServicePointer[x] = line;
+						break;
+					case 2:
+						passwordPointer[x] = line;
+						break;
+					}
+					if (lineNumber == 2) {
+						lineNumber = 0;
+						x++;
+					}
+					lineNumber++;
+				}
+				while (true) {
+					cout << "Prosze podac nazwe uslugi sieciowej, do ktorej jest potrzebne haslo:" << endl;
+					if (!(cin >> network_service2)) {
+						cin.clear();
+						cin.ignore();
+						cout << "Wystapil blad, podczas pobierania danych, prosze podac dane ponownie!" << endl;
+						getchar(); getchar();
+						system("cls");
+						continue;
+					}
+					for (int i = 0; i <= x; i++) {
+						if ((networkServicePointer[i] == "Platforma: " + network_service2)) {
+							cout << networkServicePointer[i] << endl;
+							cout << passwordPointer[i] << endl;
+							cout << "Prosze nacisnac jakikolwiek przycisk, aby wrocic do menu." << endl;
+							getchar(); getchar();
+							system("cls");
+							break;
+						}
+						if (networkServicePointer[i] != "Platforma: " + network_service2) {
+							none++;
+						}
+						if (none >= x) {
+							cout << "Podana usluga sieciowa, nie jest zapisana, lub zostala blednie wpisana, prosze wpisac nazwe uslugi ponownie." << endl;
+							cout << "Prosze nacisnac jakikolwiek przycisk, aby wrocic z powrotem do menu." << endl;
+							getchar(); getchar();
+							system("cls");
+							none = 0;
+							break;
+
+						}
+					}
+					break;
+				
+			}
+			file3.close();
+			delete[] networkServicePointer;
+			delete[] passwordPointer;
+
+		}
 
 	};
 
@@ -142,12 +235,13 @@ int main()
 {
 	Password password;
 	string platformName;
-	int number, choice, choicev2;
+	int number, choice;
 	while (true) {
 		cout << "Witaj w generatorze hasel, prosze wybrac jedna z opcji" << endl;          //struktura menu
 		cout << "1. Wygenerowanie hasla." << endl;
 		cout << "2. Odczytanie ostatnich zapisanych hasel." << endl;
-		cout << "3. Wyjscie z programu." << endl;
+		cout << "3. Odczytanie konkretnego hasla do konkretnej uslugi sieciowej." << endl;
+		cout << "4. Wyjscie z programu." << endl;
 		if (!(cin >> choice)) {
 			cin.clear();
 			cin.ignore();
@@ -189,15 +283,19 @@ int main()
 		case 2:
 			password.getFromFilePassword();                //wywolanie funkcji odczytania hasla
 			break;
-		case 3:
+		case 3:                         //wywolanie funkcji odczytania konkretnego hasla, przypietego do konkretnej uslugi sieciowej
+			password.readAndGetTwoLinesFromFile();
+			break;
+		case 4:
 			exit(0);                    //wyjscie z programu
 		}
 	}
 }
 
 //TO-DO:
-// 1. ZROB ODCZYT Z PLIKU CALEGO
-// 2. DODAJ MOZLIWOSC ZAPISYWANIA DO CZEGO SA DANE HASLA
-// 3. ODCZYT HASEL PO NAZWIE, DO KTORYCH SA PRZYPISANE
-// 4. NAPRAW PROBLEM Z POBOREM DANYCH ZE ZNAKAMI BIALYMI. <- nie da sie
-// 5. DODAJ DZIALANIE NA WSKAZNIKACH, W RAZIE ODCZYTU WIELU HASEL Z PLIKU, (KONKRETNYCH HASEL), DO USPRAWNIENIA APLIKACJI
+// 1. ODCZYT HASEL PO NAZWIE, DO KTORYCH SA PRZYPISANE.
+// 2. ZMIEN KOD TAK, ABY PROGRAM MOGL OD RAZU WCZYTAC CALA ZAWARTOSC PLIKU, ZEBY TEGO NIE ROBIL CO KAZDA PROSBE ODCZYTANIA CZEGOKOLWIEK Z PLIKU.
+// 3. NAPRAW PROBLEM Z POBOREM DANYCH ZE ZNAKAMI BIALYMI. <- nie da sie
+// 4. DODAJ DZIALANIE NA WSKAZNIKACH, W RAZIE ODCZYTU WIELU HASEL Z PLIKU, (KONKRETNYCH HASEL), DO USPRAWNIENIA APLIKACJI.
+// 5. NAUCZ SIE WYKRYWANIE PASSWORD BOXOW, ABY MOC TO WYKORZYSTAC W PRZYSZLOSCI W TYM KODZIE.
+
